e440fd785ccb0194123c5b78fa959a5630df0954
csugulo <csugulo@gmail.com>
2019-01-21 16:17:13 +0800

    change project structure

---
+++ coding_interview.h
@@ -0,0 +1,17 @@
+//
+// Created by xieyang on 19-1-21.
+//
+
+#ifndef CODING_INTERVIEW_CODING_INTERVIEW_H
+#define CODING_INTERVIEW_CODING_INTERVIEW_H
+
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <algorithm>
+
+#include <cstring>
+
+using namespace std;
+
+#endif //CODING_INTERVIEW_CODING_INTERVIEW_H
